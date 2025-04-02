16e1668dcd730517cd6db1bb372881f44a60396b
shakib-shajid <shakibalshajid1682@gmail.com>
2025-04-02 21:22:03 +0600

    newsletter added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./src/**/*.{html,js}"],
-//   theme: {
-//     extend: {},
-//   },
-//   // plugins: [require("daisyui")],
-// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+        screens: {
+            'custom-1440': '1440px', // Custom breakpoint for 1080px
+            'custom-1600': '1600px', // Custom breakpoint for 1080px
+          },
+    },
+  },
+};
